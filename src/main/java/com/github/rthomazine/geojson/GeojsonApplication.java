173bc2a6832defdc1618f006b1ea6200348d54eb
Renato M Thomazine <rthomazine@uolinc.com>
2019-10-14 22:28:02 -0300

    Initial commit

---
+++ src/main/java/com/github/rthomazine/geojson/GeojsonApplication.java
@@ -0,0 +1,13 @@
+package com.github.rthomazine.geojson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeojsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeojsonApplication.class, args);
+	}
+
+}
