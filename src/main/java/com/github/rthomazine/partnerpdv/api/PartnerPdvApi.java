56705f7ab024f6f6fa281b0c1b2ad419f86ddb98
Renato M Thomazine <rthomazine@uolinc.com>
2019-10-24 12:10:48 -0300

    Added a services layer

--- src/main/java/com/github/rthomazine/partnerpdv/api/PartnerPdvApi.java
+++ src/main/java/com/github/rthomazine/partnerpdv/api/PartnerPdvApi.java
@@ -1,8 +1,10 @@
 package com.github.rthomazine.partnerpdv.api;
 
+import com.github.rthomazine.partnerpdv.api.exception.DocumentException;
 import com.github.rthomazine.partnerpdv.api.exception.NotFoundException;
-import com.github.rthomazine.partnerpdv.persistence.PartnerPdv;
-import com.github.rthomazine.partnerpdv.persistence.PartnerPdvRepository;
+import com.github.rthomazine.partnerpdv.api.model.PartnerPdv;
+import com.github.rthomazine.partnerpdv.api.model.PartnersPdvs;
+import com.github.rthomazine.partnerpdv.service.PartnersPdvsService;
 import io.swagger.annotations.Api;
 import mil.nga.sf.geojson.Position;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,33 +13,37 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping(path = "/api")
 @Api(tags = "PartnerPDV")
 public class PartnerPdvApi {
 
     @Autowired
-    private PartnerPdvRepository partnerPdvRepository;
+    private PartnersPdvsService partnersPdvsService;
 
-    @PostMapping(path = "/partner/pdv", produces = { MediaType.APPLICATION_JSON_VALUE })
+    @PostMapping(path = "/partners", produces = { MediaType.APPLICATION_JSON_VALUE })
     @ResponseStatus(HttpStatus.CREATED)
-    public PartnerPdv createPartnerPdv(@RequestBody PartnerPdv partnerPdv) throws DuplicateKeyException {
-        return partnerPdvRepository.save(partnerPdv);
+    public PartnerPdv createPartnerPdv(@RequestBody PartnerPdv partnerPdv) throws DocumentException, DuplicateKeyException {
+        return partnersPdvsService.createPartnerPdv(partnerPdv);
     }
 
-    @GetMapping(path = "/partner/{id}", produces = { MediaType.APPLICATION_JSON_VALUE })
+    @GetMapping(path = "/partners", produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseStatus(code = HttpStatus.OK)
+    public PartnersPdvs getAllPartnersPdvs() {
+        return partnersPdvsService.getAllPdvs();
+    }
+
+    @GetMapping(path = "/partners/{id}", produces = { MediaType.APPLICATION_JSON_VALUE })
     @ResponseStatus(code = HttpStatus.OK)
     public PartnerPdv getPartnerPdvById(@PathVariable String id) throws NotFoundException {
-        Optional<PartnerPdv> partnerPdv = partnerPdvRepository.findById(id);
-        return partnerPdv.orElseThrow(() -> new NotFoundException(String.format("No partner pdv found with id %s", id)));
+        return partnersPdvsService.getPdvById(id);
     }
 
-    @GetMapping(path = "/partner/search", produces = { MediaType.APPLICATION_JSON_VALUE })
+    @GetMapping(path = "/partners/search", produces = { MediaType.APPLICATION_JSON_VALUE })
     @ResponseStatus(code = HttpStatus.OK)
     public PartnerPdv searchPartnerPdvByLocation(@RequestBody Position location) throws NotFoundException {
         //TODO
+
         return null;
     }
 
